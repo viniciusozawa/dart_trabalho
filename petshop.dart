92ae73097d49c5c0edfaf08399a8eb50ad2538e1
Vinicius <yudiozawa123@gmail.com>
2026-03-19 09:41:54 -0300

    create project

---
+++ petshop.dart
@@ -0,0 +1,6 @@
+void main(){
+  print("\tSejam Bem-Vindos a loja Cuidapet! \n\tEm breve teremos um sistema de autoatendimento.");
+
+  print("\nLocalização da loja: \n\tRua dos Petshop, 123 - Bairro dos Animais - Cidade dos Bichos");
+}
+
