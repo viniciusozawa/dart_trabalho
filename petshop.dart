9fe8362a2236200c5203d2cf48d0a3e64da5b2d3
Japola <yudiozawa123@gmail.com>
2026-04-03 13:37:31 -0300

    feat: Fase 5: implementação das escolhas 3 e 4

--- petshop.dart
+++ petshop.dart
@@ -5,7 +5,7 @@ void main(){
 
   String? usuario;
 
-  print("Digite seu nome: ");
+  stdout.write("Digite seu nome: ");
   usuario = stdin.readLineSync();
 
 
@@ -14,7 +14,7 @@ void main(){
 
   print("\nLocalização da loja: \n\t$endereco");
   print("\nTelefone: \n\t$telefone");
-  print("\n\n\t1 - Ver ofertas de Produtos  \n\t2 - Ver ofertas de Serviços.\n\nDigite o número da opção desejada: ");
+  stdout.write("\n\n\t1 - Ver ofertas de Produtos  \n\t2 - Ver ofertas de Serviços. \n\t3 - Ver ofertas de roupas \n\t4 - Ver novos serviços.\n\nDigite o número da opção desejada: ");
   String? opcao = stdin.readLineSync();
 
   switch (opcao) {
@@ -24,8 +24,15 @@ void main(){
     case '2':
       print("Banho e tosa na promoção pelo preço do banho R\$ 54,00");
       break;
+    case '3':
+      print("Roupas em oferta - Capa de chuva R\$59,99");
+      break;
+    case '4':
+      print("\n\n\t| Hidratação de pelo R\$ 39,99 \n\t| Tosa higiênica por R\$ 10,99 \n\t| Tingimento dos pelo por R\$ 55,99.");
+      break;
+
     default:
-      print("\nOpção inválida. Por favor, escolha 1 ou 2.");
+      print("\nOpção inválida. Por favor, escolha 1, 2, 3 ou 4.");
   }
 
   print("\n\nOferecemos e nossa loja produtos e serviços para seu pet. Para venda de produtos, procure o coloborador junior e, para serviços como banho ou tosa, procure a colaboradora Ana. Obrigado e esperamos que tenha uma ótima experiência em nossa loja!");
